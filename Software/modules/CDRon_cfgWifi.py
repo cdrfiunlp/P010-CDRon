9a0e4405910a8f3ea96cad506499671d2dac0782
Jorge Anderson <anderson.jorgeluis@gmail.com>
2016-03-19 20:01:51 -0300

    v1 WIFI config

--- Software/modules/CDRon_cfgWifi.py
+++ Software/modules/CDRon_cfgWifi.py
@@ -7,6 +7,14 @@ def configure_wifi(fd_serial):
 	print "Initialized Wifi configuration...\n"
 	SSID= raw_input("-> SSID: ")
 	PASS= getpass.getpass("-> Password: ")
-	fd_serial.write("startup config\n")
-	while(1):
-		time.sleep(0.1)
+	fd_serial.write("1\n" + SSID + "\n" + PASS + "\n")
+	time.sleep(0.1)
+	if(fd_serial.inWaiting() != 0):
+	        receive = fd_serial.readline()
+		if receive == "OK\n":
+			print("Wifi configured correctly")
+		else:
+			print("ERROR... Wifi can not be configured correctly\n\n")
+	else:
+		print("ERROR... Wifi can not be configured correctly\n\n")
+	
