811e7b5a92e91f4f611ab6e741e7876bd82d6a30
Jorge Anderson <anderson.jorgeluis@gmail.com>
2016-03-22 21:05:17 -0300

    +imu test

--- Software/modules/CDRon_tstimu.py
+++ Software/modules/CDRon_tstimu.py
@@ -4,23 +4,19 @@ import getpass
 import serial
 
 def tst_imu(fd_serial):
-	print "Initialized IMU test...\n"
-
-	fd_serial.write("3\n")
-	time.sleep(0.1)
-	if(fd_serial.inWaiting() != 0):
-	        receive = fd_serial.readline()
-		if receive == "OK\n":
-			try:
-				print("Reading IMU...\n")
-				while(True):
-					if(fd_serial.inWaiting() != 0):
-						receive = fd_serial.readline()
-						print receive
-			except KeyboardInterrupt:
-				print("Finished reading Imu.\n")
+   print "Initialized IMU test...\n"
+   try:
+	while(True):
+		time.sleep(1)
+		fd_serial.flushInput()
+		fd_serial.write("3\n")
+		time.sleep(0.1)
+		if(fd_serial.inWaiting() != 0):
+			receive = fd_serial.readline()
+			receive = receive.split(':')
+			print "Yaw: " + receive[0] + " || Pitch: " + receive[1] + " || Roll: " + receive[2]
 		else:
-			print("ERROR... Connection cannot be established.\n\n")
-	else:
-		print("ERROR... Connection cannot be established.\n\n")
+			print("ERROR... Cannot be read the IMU.\n\n")
+   except KeyboardInterrupt:
+	print("Finished reading Imu.\n")
 	
